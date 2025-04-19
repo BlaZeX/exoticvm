85054495d4c644ca4975b9b0a4ba67e1dfee5f84
BlaZeX <shashankbadgamer@gmail.com>
2025-04-19 22:44:48 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: cloudflare({
-    mode: 'directory'
-  })
+  output: 'static',
+  integrations: [tailwind(), react()]
 });
